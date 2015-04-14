ac952d916a3fd12326824dafc7e635613d71c244
Rafal Sladek <rafal.sladek@gmail.com>
2015-04-14 02:08:15 +0200

    added some logic

---
+++ SimpleChat/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SimpleChat
+{
+    public partial class Form1 : Form
+    {
+        private IHubProxy chat;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var hubConnection = new HubConnection("http://localhost:1806/");
+            chat = hubConnection.CreateHubProxy("chatHub");
+            chat.On<string>("newMessage",
+                msg => lbMessages.Invoke(
+                    new Action(() => lbMessages.Items.Add(msg))));
+
+            hubConnection.Start().Wait();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chat.Invoke<string>("sendMessage", tbMessage.Text);
+        }
+    }
+}
